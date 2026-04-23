bb96d36ef3eb9c1a9207bb15316398d28c2d895f
Ojaswa Varshney <118374998+JrG-One@users.noreply.github.com>
2026-04-23 23:52:20 +0530

    chore: prepare for deployment and fix type errors

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
       {
         source: '/2026/:path*',
         destination: 'https://www.comsnets.org/archive/2026/:path*',
+        permanent: false,
       },
       {
         source: '/archive/:path*',
         destination: 'https://www.comsnets.org/archive/:path*',
+        permanent: false,
       },
       {
         source: '/assets/:path*',
-        destination: 'https://www.comsnets.org/assets/:path*'
+        destination: 'https://www.comsnets.org/assets/:path*',
+        permanent: false,
       }
     ];
   },
